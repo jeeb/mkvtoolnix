8d593bd733fa954889440964edecdf7a0f8f594f
caustik <Aaron.Robinson@rovicorp.com>
2013-05-21 17:35:31 -0400

    Rovi version

--- src/common/version.cpp
+++ src/common/version.cpp
@@ -25,7 +25,7 @@
 #include "common/strings/parsing.h"
 #include "common/version.h"
 
-#define VERSIONNAME "Old Devil"
+#define VERSIONNAME "Old Devil" " Rovi v1.0.0"
 
 version_number_t::version_number_t()
   : valid(false)
